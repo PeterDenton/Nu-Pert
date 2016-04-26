ba18d44839fb2a564fa45bcf4f412c58f4d3e9f3
Peter Denton <peterbd1@gmail.com>
2016-04-26 13:14:59 -0500

    added more docs in Examples

--- src/Examples.cpp
+++ src/Examples.cpp
@@ -10,21 +10,24 @@
 // Calculates P(nubar_mu->nubar_e) for DUNE parameters
 void Example1()
 {
+	std::cout << "==== Example 1 ====" << std::endl;
+
 	int alpha = 1;					// muon type neutrino
 	int beta = 0;					// electron type neutrino
-	double delta = 3 * M_PI / 2;	// the CP phase
+	double delta = 3 * M_PI / 2;	// The CP phase
 	int exp_num = 0;				// 0 - DUNE, 1 - NOvA, 2 - T2K(HK)
 	double E = 2.5;					// neutrino energy in GeV
 	int order = 0;					// Zeroth order in the perturbative expansion
 	E *= -1;						// Negative energy for antineutrinos
 
 	double Yrho, L;
-	set_experimental_parameters(exp_num, &Yrho, &L); // get matter density and baseline for the experiment
+	set_experimental_parameters(exp_num, &Yrho, &L); // Get matter density and baseline for the experiment
 
 	double a = Yrho * E * Y_to_a;   // Y_to_a converts units
 	double LE = L / E;
-	double P = GF::Palphabeta(alpha, beta, a, LE, delta, order);
-	std::cout << std::setprecision(10);
+	double P = GF::Palphabeta(alpha, beta, a, LE, delta, order); // Calculate the transition probability using the general form described in the tables
+
+	std::cout << std::setprecision(10); // Increases the number of decimal places to output
 	std::cout << "P(nubar_mu->nubar_e) at zeroth order: ";
 	std::cout << P << std::endl;
 
